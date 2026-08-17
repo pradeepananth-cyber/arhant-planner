3a1b03647e800fa167fbf78d31914c1a6c0ec56c
Pradeep Ananth <panantha@Pradeeps-MacBook-Air-2.local>
2026-08-17 10:30:49 -0700

    Storage functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './arhant-planner.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Planner from "./Planner.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(<Planner />);
